0b1ea25fa20dd19848836bdd5b6926d00c0eef39
Kakada Chheang <kakada.chheang@gmail.com>
2014-11-11 15:19:17 +0700

    remove normalize channel from schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,15 +4,10 @@ class Schedule < ActiveRecord::Base
 
   belongs_to :group
   
-  before_save :normalize_channels
   before_save :repeatable
 
   def repeatable
     self.is_repeated ? (self.start_date = nil) : (self.conditions = []) 
-  end
-
-  def normalize_channels
-    self.channels = self.channels.map{|c| Api::Channel.new(id: c[:id], name: c[:name])}
   end
 
 end
